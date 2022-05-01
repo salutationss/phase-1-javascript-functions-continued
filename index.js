ca4401f5ccdce36b4661b9ede4e6a4a5bf096958
Alex Blackburn <Alex>
2022-05-01 00:08:55 -0400

    commit tree 5c98b689fcb4d597339c1e431d94c1f49226dc32

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+
+}
+
+function mondayWork(verb = "go to the office") {
+    return `This Monday, I will ${verb}.`
+}
+
+let wrapAdjective = function(yo = "*") {
+    return function(wavy = "special") {
+        return `You are ${yo}${wavy}${yo}!`
+    }
+}
